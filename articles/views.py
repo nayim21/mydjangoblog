25322414e0fcc4a32fec7b8504295d8365690170
nayim21 <sahel_naseh21@yahoo.com>
2021-01-24 19:20:22 +0330

    url capture

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 from . import models
 # Create your views here.
 def article_list(request):
     artcvalue=models.Article.objects.all().order_by('date')
     return render(request,'articlelist.html',{'xartc':artcvalue})
+def article_detail (request , slug) :
+    return HttpResponse (slug)
