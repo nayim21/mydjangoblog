cd8d5ea0c6d5504f1635b706453a6e846622cef3
nayim21 <sahel_naseh21@yahoo.com>
2021-01-24 19:51:42 +0330

    detail url capture

--- articles/views.py
+++ articles/views.py
@@ -6,4 +6,6 @@ def article_list(request):
     artcvalue=models.Article.objects.all().order_by('date')
     return render(request,'articlelist.html',{'xartc':artcvalue})
 def article_detail (request , valslug) :
-    return HttpResponse (valslug)
+#    return HttpResponse (valslug)
+    valarticle=models.Article.objects.get(slug=valslug)
+    return render(request,'articledetail.html',{'valarticle2':valarticle})
